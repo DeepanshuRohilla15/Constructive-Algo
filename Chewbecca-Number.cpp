738e31a7d7be48f169aefa4123c8602486a4e8d1
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-26 12:58:12 +0530

    Constructive

---
+++ Chewbecca-Number.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string x;
+    cin>>x;
+    
+    for(auto &digit : x)
+    {
+        if(digit > '4')
+        {
+            digit = '9' - digit + '0';
+        }
+    }
+    if(x.front() == '0')
+    {
+        x.front() = '9';
+    }
+    cout<<x<<"\n";
+}
