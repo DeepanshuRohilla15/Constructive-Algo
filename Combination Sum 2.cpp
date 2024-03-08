246831c079a33d90f91da78b2745072b65e6abf3
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-03-08 21:54:51 +0530

    Create Combination Sum 2.cpp

---
+++ Combination Sum 2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+void find(int ind, int target, vector<int> & arr, vector<int> &ds, vector<vector<int>> &ans)
+{
+	if(target == 0)
+	{
+		ans.push_back(ds);
+		return;
+	}
+
+	for(int i = ind; i < arr.size(); i++)
+	{
+		if(i > ind && arr[i] == arr[i-1]) continue;
+		if(arr[i] > target) break;\
+		ds.push_back(arr[i]);
+		find(i+1, target - arr[i], arr, ds, ans);
+		ds.pop_back();
+
+	}
+}
+
+vector<vector<int>> combinationSum2(vector<int> &arr, int n, int target){
+	sort(arr.begin(), arr.end());
+	vector<vector<int>> ans;
+	vector<int> ds;
+	find(0, target, arr, ds, ans);
+	return ans;
+
+}
