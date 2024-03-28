0d7ed4a9e8681df9eedd57bcb7758a63bdb8e889
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-03-28 23:22:57 +0530

    Create String task.cpp

---
+++ String task.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s, k;
+    cin>>s;
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u' && s[i] != 'y' && s[i] != 'A' && s[i] != 'E' && s[i] != 'O' && s[i] != 'I' && s[i] != 'U' && s[i] != 'Y' )
+        {
+            k = k+'.';
+            k = k+(char) tolower(s[i]);
+        }
+    } 
+     cout<<k;
+}
