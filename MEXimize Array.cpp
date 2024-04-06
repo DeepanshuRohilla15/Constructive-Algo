b02afe542a521731411e8e94e6b9daa379c9f008
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-04-06 21:44:09 +0530

    Create MEXimize Array.cpp

---
+++ MEXimize Array.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string>
+#include <bitset>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+#include <iomanip>
+#include <climits>
+#include <assert.h>
+#include <chrono>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define vii vector<ll>
+#define pii pair<ll, ll>
+#define vvi vector<vii>
+#define vpi vector<pair<ll, ll>>
+#define mll map<ll, ll>
+#define pql priority_queue<ll, vii>
+#define pqg priority_queue<ll, vii, greater<ll>>
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) x.size();
+#define yn(flag) cout << (flag ? "YES" : "NO");
+
+using namespace std;
+
+void solution()
+{
+    // write code
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) cin>>v[i];
+
+    long long ans = 0;
+    for(int i = 0; i < n; i++)
+    {
+        ans += abs(v[i] - i);
+    }
+    cout<<ans<<"\n";
+
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll tt = 1;
+    cin >> tt;
+    ll t = 1;
+    while (t <= tt)
+    {
+        
+        solution();
+        t++;
+    }
+    return 0;
+}
