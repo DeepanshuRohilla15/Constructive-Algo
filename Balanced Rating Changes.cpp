3b2ee7b1bfd6459a545fd726a71907ae48463e7c
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-04-08 23:53:30 +0530

    Create Balanced Rating Changes.cpp

---
+++ Balanced Rating Changes.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    int odd = 0;
+    for(int i = 0; i < n; i++)
+    {
+        cin>>v[i];
+        if(v[i]%2) odd++;
+    }
+    
+    odd = odd/2;
+    for(auto x : v)
+    {
+        if(x%2)
+        {
+            if(odd > 0) odd--, x--;
+            else x++;
+        }
+        cout<<x/2<<"\n";
+    }
+}
