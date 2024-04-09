dbb30f90fd25cfe88a0e0e9003b3ce661e80d146
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-04-09 14:57:27 +0530

    Create Ichihime and Triangle.cpp

---
+++ Ichihime and Triangle.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b, c, d;
+        cin>>a>>b>>c>>d;
+        
+        cout<<b<<" "<<c<<" "<<c<<"\n";
+        
+    }
+}
