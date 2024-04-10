985dc9e27d1196e6bec80e2b6936dec7f3f2ed7e
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-04-10 11:21:30 +0530

    Create Array with Odd Sum.cpp

---
+++ Array with Odd Sum.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n, x;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        int odd = 0, even = 0, sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            cin>>x;
+            sum += x;
+            if(x%2) odd++;
+            else even++;
+        }
+        
+        if(sum % 2) cout<<"YES\n";
+        else
+        {
+            if(odd >=1 && even >= 1)
+            {
+                cout<<"YES\n";
+            }
+            else
+            {
+                cout<<"NO\n";
+            }
+        }
+    }
+}
