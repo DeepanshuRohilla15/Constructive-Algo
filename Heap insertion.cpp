ae3bf15cd703f95bdbebe51d6395dc8149ec4ac0
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-06-10 14:45:56 +0530

    Create Heap insertion.cpp

---
+++ Heap insertion.cpp
@@ -0,0 +1,31 @@
+class heap
+{
+  public: 
+    int arr[100];
+    int size;
+
+    heap(){
+      arr[0] = -1;
+      size = 0;
+    }
+
+    void insert(int val)
+    {
+      size = size + 1;
+      int index = size;
+      arr[size] = val;
+
+      while(index > 1){
+        int parent = index / 2;
+
+        if(arr[parent] < arr[index]){
+          swap(arr[parent], arr[index]);
+          index = parent;
+        }
+        else 
+        {
+          return;
+        }
+      } 
+    }
+}
