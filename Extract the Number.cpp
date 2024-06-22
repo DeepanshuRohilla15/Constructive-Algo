5f760154fc444a2a70edceb4f4684e49528f5844
Deepanshu Rohilla <97741113+DeepanshuRohilla15@users.noreply.github.com>
2024-06-22 21:06:01 +0530

    Create Extract the Number.cpp

---
+++ Extract the Number.cpp
@@ -0,0 +1,21 @@
+long long count=0,n=0;
+        bool indicate=false;
+        for(char x:sentence)
+        {
+            if(x>='0' && x<='9')
+            {
+                n=n*10+(x-'0');
+                if(x=='9')
+                indicate=true;
+            }
+            else if(x==' ')
+            {
+                if(!indicate)
+                count=max(count,n);
+                indicate=false;
+                n=0;
+            }
+        }
+        if(indicate==false)
+        count=max(count,n);
+        return count==0?-1:count;
